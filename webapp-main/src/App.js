87e06f74953517453997d77bf97f68fc702ab3a1
Igor Zalutski <izalutski@gmail.com>
2020-05-12 00:19:54 +0100

    Connect webapp with backend

--- webapp-main/src/App.js
+++ webapp-main/src/App.js
@@ -2,15 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>API root: {process.env.REACT_APP_API_ROOT}</p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ''
+    };
+  }
+
+  componentDidMount() {
+    //TODO: handle errors properly
+    fetch(process.env.REACT_APP_API_ROOT + 'message')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ message: res.message })
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {this.state.message}
+        </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
