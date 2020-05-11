a1db6c21c572b262553edf59081738ae33ee3a4d
Igor Zalutski <izalutski@gmail.com>
2020-05-11 19:09:22 +0100

    Add /message endpoint, read env var

--- backend-main/routes/index.js
+++ backend-main/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/message', function(req, res, next) {
+  res.status(200).send({
+    message: process.env.DG_MESSAGE || "Hello World"
+  });
+});
+
 module.exports = router;
